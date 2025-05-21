ce640786757310ac94383eb95f73e2558a60bdb6
Ali Alper sever <alialpersever@Ali-MacBook-Air.local>
2025-05-21 16:56:00 +0300

    test

--- src/main/java/com/example/simpleapp/HelloController.java
+++ src/main/java/com/example/simpleapp/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.simpleapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHello() {
-        return "Merhaba! Spring Boot uygulamam Kubernetes uzerinde calisiyor!";
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Merhaba! Spring Boot uygulamam Kubernetes üzerinde çalışıyor!");
+        return "index";
     }
-} 
+}
